81113128cdb3cfbda81546ad9928ab1f0a4b608f
Mark Brightman <mark.brightman@ravelin.com>
2021-07-14 18:47:42 +0100

    add dependencies

--- Package.swift
+++ Package.swift
@@ -10,6 +10,10 @@ let package = Package(
             name: "RavelinEncrypt", 
             targets: ["RavelinEncrypt"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.1"),
+        .package(url: "https://github.com/airsidemobile/JOSESwift.git", from: "2.4.0"),
+    ],
     targets: [
         .binaryTarget(
             name: "RavelinEncrypt", 
